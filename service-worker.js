f634b9308c237eeb195269dbebcc0e85ceccc329
johnstonpc <102391762+johnstonpc@users.noreply.github.com>
2023-05-25 22:18:35 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "meu-pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  '/index.html',
+  '/numeros.html',
+  '/cores.html',
+  '/estilo.css',
+  '/funcaos1.js',
+  'img/ingles.png',
+  'https://johnstonpc.github.io/app-ingles/'
+   
+   
+];
+
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Cache aberto");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log("Retornando do cache: ", event.request.url);
+        return response;
+      }
+
+      console.log("Buscando da rede: ", event.request.url);
+      return fetch(event.request);
+    })
+  );
+});
